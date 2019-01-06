79573fbff2a693b9f0e4a4ae97c85ae0d541c84f
Hugo Picollet <hugo.picollet@gmail.com>
2019-01-06 11:55:06 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringMySQLDemo/SpringMySqlDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringMySQLDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMySqlDemoApplication.class, args);
+	}
+
+}
+
